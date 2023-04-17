033353158164ae2930276f123568afa8bcd04818
root <patrickltran93@gmail.com>
2023-04-17 05:14:48 +0000

    updated charts

--- ecom_analysis.py
+++ ecom_analysis.py
@@ -5,31 +5,31 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 #%%
-df = pd.read_excel('/home/docker/ecommerce/ecom.xlsx', sheet_name='E Comm')
+df = pd.read_excel('/home/docker/ecommerce/ecom_analysis/ecom.xlsx', sheet_name='E Comm')
 df_c = df[df['Churn'] == 1]
 df_nc = df[df['Churn'] == 0]
 
 # %%
 df.describe()
-df_id = df.pop('CustomerID')
-churn_values = df.pop('Churn')
+#df_id = df.pop('CustomerID')
+#churn_values = df.pop('Churn')
 
-df_churned = df.
 
 #%%
-sns.countplot(x='Churn',data=df,)
+sns.countplot(x='Churn',data=df)
 plt.title('No Churn vs Churned')
 plt.xlabel('No Churn: 0  Vs  Churned: 1')
 plt.ylabel('Count')
 
 #%%
 fig, ax = plt.subplots(3,4,figsize=(20, 18))
-ax.flatten()
+fig.suptitle('Density of Numeric Features by Churn', fontsize=20)
+ax = ax.flatten()
+
 for i in df.columns[2:]:
-    sns.kdeplot(data=df_c[i], label='Churned', linewidth=3)
-    sns.kdeplot(data=df_nc[i], label='No Churn', linewidth=3)
-    plt.xlabel(i)
-    plt.ylabel('Density')
+    if df.dtypes[i] != np.dtype('object'):
+        sns.kdeplot(data=df_c[i], label='Churned', linewidth=3)
+        sns.kdeplot(data=df_nc[i], label='No Churn', linewidth=3)
 plt.show()
 
 
