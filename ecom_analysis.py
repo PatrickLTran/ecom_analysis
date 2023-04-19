045331a34d3798cd1b217a8828ad9fca19a32301
root <patrickltran93@gmail.com>
2023-04-19 05:42:47 +0000

    added bivariate distribution analysis

--- ecom_analysis.py
+++ ecom_analysis.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
 #%%
 df = pd.read_excel('/home/docker/ecommerce/ecom_analysis/ecom.xlsx', sheet_name='E Comm')
@@ -24,9 +25,10 @@ plt.ylabel('Count')
 #%%
 # select columns not object
 df_cv = df.select_dtypes(exclude='object')
-df_cv
+df_cat = df.select
 #%%
-
+# Density distribution plots  
+ 
 fig, ax = plt.subplots(3,4,figsize=(20, 18))
 fig.suptitle('Density of Numeric Features by Churn', fontsize=20)
 ax = ax.flatten()
@@ -34,6 +36,8 @@ ax = ax.flatten()
 for idx,col in enumerate(df_cv.columns[2:]):
     sns.kdeplot(data=df_c[col], label='Churned', linewidth=3, ax=ax[idx] )
     sns.kdeplot(data=df_nc[col], label='No Churn', linewidth=3, ax=ax[idx])
+    ax[idx].legend(loc='upper right')
+
 plt.show()
 
 
@@ -42,6 +46,10 @@ plt.show()
 df_id = df.pop('CustomerID')
 churn_values = df.pop('Churn')
 # %%
+sns.jointplot(x=df_c['Tenure'], y = df_c['CouponUsed'], kind='kde', cmap='Blues')
+#%%
+sns.jointplot(x=df_nc['Tenure'], y = df_nc['CouponUsed'], kind='kde', cmap='Blues')
 
 
 
+# %%
